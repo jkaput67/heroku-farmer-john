d72f9d8b573e4e246a782c9eb3abb01fd0fcf0d8
Tony Buckingham <tony@zeitcode.com>
2020-01-13 16:11:20 -0600

    move pinterest and myxx tags to theme header

--- wp-content/themes/farmerjohn/header.php
+++ wp-content/themes/farmerjohn/header.php
@@ -37,6 +37,25 @@
 		  gtag('config', 'UA-33831844-1');
 		</script>
 
+		<script type='text/javascript' src='https://mini.myxxrecipes.com/api/lp/launchpad.js'></script>
+
+		<!-- Pinterest Tag -->
+		<script>
+		!function(e){if(!window.pintrk){window.pintrk = function () {
+		window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
+		n=window.pintrk;n.queue=[],n.version="3.0";var
+		t=document.createElement("script");t.async=!0,t.src=e;var
+		r=document.getElementsByTagName("script")[0];
+		r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+		pintrk('load', '2613561171997', {em: '<user_email_address>'});
+		pintrk('page');
+		</script>
+		<noscript>
+		<img height="1" width="1" style="display:none;" alt=""
+		src="https://ct.pinterest.com/v3/?event=init&tid=2613561171997&pd[em]=<hashed_email_address>&noscript=1" />
+		</noscript>
+		<!-- end Pinterest Tag -->
+
 		<!--[if IE 9]>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
 		<![endif]-->
