e16e646096184d8b3f122559ed5ad6cc9cc4f995
Joe Porto <cthulhu0@gmail.com>
2020-05-28 15:31:04 -0400

    update video

--- wp-content/themes/farmerjohn/parts/landing-bresee.php
+++ wp-content/themes/farmerjohn/parts/landing-bresee.php
@@ -12,7 +12,7 @@
 			</div>
 		</div>
 		<div class="column small-12 large-6 tip iframe_tip" style="background-color: black;">
-				<iframe class="absolute-block" src="https://player.vimeo.com/video/403472870" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+				<iframe class="absolute-block" src="https://player.vimeo.com/video/398514256" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 		</div>
 	</section>
 	<div class="clearfix"></div>
